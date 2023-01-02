2737ca4b1e49126cf0406acbbd68c36e16231491
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-02 18:57:11 +0100

    Le fichier de la class User

---
+++ User.php
@@ -0,0 +1,51 @@
+<?php
+class User{
+    private $id;
+    public $login;
+    private $password;
+    public $email;
+    public $firstname;
+    public $lastname;
+
+    // La BDD.
+    private $servername = 'localhost';
+    private $username_b = 'root';
+    private $password_b = '';
+    private $database = 'classes';
+
+    private $bdd;
+
+    public function __construct(){
+        //  La connexion a la BDD.
+        $this->bdd = new mysqli($this->servername, $this->username_b, $this->password_b, $this->database);
+        // $this->bdd = new mysqli('localhost', 'root', '', 'classes');
+    }
+
+    public function register($login, $password, $email, $firstname, $lastname){
+    
+        $this->bdd->query("INSERT INTO `utilisateurs`(`login`, `password`, `email`, `firstname`, `lastname`) VALUES ('$login', '$password', '$email', '$firstname', '$lastname');");
+    }
+
+    public function connect(){
+
+    }
+
+    public function disconnect(){
+
+    }
+
+    public function delete(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function isConnected(){
+
+    }
+
+
+
+}
